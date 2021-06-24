22e86e4f79a71013f727fe66aba657b9ad21aca7
princekhj555 <princekhj555@gmail.com>
2021-06-24 11:19:02 +0530

    Day 40

---
+++ 5.LinkedList/33.cpp
@@ -0,0 +1,94 @@
+/* to delete which have greater value on the right side of the linkedlist */
+
+// { Driver Code Starts
+#include<bits/stdc++.h>
+
+using namespace std;
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+
+
+void print(Node *root)
+{
+    Node *temp = root;
+    while(temp!=NULL)
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    }
+}
+
+
+
+ // } Driver Code Ends
+/*
+
+The structure of linked list is the following
+
+struct Node
+{
+    int data;
+    Node* next;
+    
+    Node(int x){
+        data = x;
+        next = NULL;
+    }
+};
+*//*#################################################################################################################3*/
+class Solution
+{
+    public:
+    Node *compute(Node *head)
+    {
+        if(head==NULL or head->next==NULL) return head;
+       Node *temp=compute(head->next);
+       if(temp->data>head->data) return temp;
+       head->next=temp;
+       return head;
+    }
+    
+};
+   
+/*#######################################################################################################################33*/
+// { Driver Code Starts.
+
+int main()
+{
+    int T;
+	cin>>T;
+
+	while(T--)
+	{
+		int K;
+		cin>>K;
+		struct Node *head = NULL;
+        struct Node *temp = head;
+
+		for(int i=0;i<K;i++){
+		    int data;
+		    cin>>data;
+			if(head==NULL)
+			    head=temp=new Node(data);
+			else
+			{
+				temp->next = new Node(data);
+				temp = temp->next;
+			}
+		}
+        Solution ob;
+        Node *result = ob.compute(head);
+        print(result);
+        cout<<endl;
+    }
+}
+  // } Driver Code Ends
