c56918f8b90fccaf2d0799e9fc9e258b14fad378
princekhj555 <princekhj555@gmail.com>
2021-09-03 14:52:05 +0530

    Day 107

---
+++ Leetcode Practice/242e.cpp
@@ -0,0 +1,21 @@
+/*242. Valid Anagram*/
+
+class Solution {
+public:
+    bool isAnagram(string s, string t) {
+        unordered_map<char,int> ump;
+        int n1=s.length();
+        int n2=t.length();
+        if(n1!=n2) return false;
+        int chk[26] = {0};
+        for(int i=0;i<n1;i++)
+        {
+            chk[s[i]-'a']++;
+            chk[t[i]-'a']--;
+        }
+        for(auto i:chk){
+            if(i!=0) return false;
+        }
+        return true;
+    }
+};
--- README.md
+++ README.md
@@ -1,12 +1,12 @@
 # DSA
 
-#Day 106
+#Day 107
 
 Love babbar sheet
     100/450
     
 leetcode   
-easy : 42
+easy : 43
 medium: 16
 hard: 2
 
