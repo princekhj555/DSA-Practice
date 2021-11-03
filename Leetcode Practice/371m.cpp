d52c05cca93b693263f86dc06496f6d2a0fa4080
princekhj555 <princekhj555@gmail.com>
2021-11-03 12:52:11 +0530

    Day 168

---
+++ Leetcode Practice/371m.cpp
@@ -0,0 +1,23 @@
+//371. Sum of Two Integers
+class Solution {
+public:
+    int getSum(int x, int y) {
+         while (y != 0)
+    {
+        // carry should be unsigned to
+        // deal with -ve numbers
+        // carry now contains common
+        //set bits of x and y
+        unsigned carry = x & y;
+ 
+        // Sum of bits of x and y where at
+        //least one of the bits is not set
+        x = x ^ y;
+ 
+        // Carry is shifted by one so that adding
+        // it to x gives the required sum
+        y = carry << 1;
+    }
+    return x;
+    }
+};
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # DSA
 
-#Day 167
+#Day 168
 
 Love babbar sheet
     106/450
     
 leetcode   
 easy : 74
-medium: 69
-hard: 5
+medium: 70
+hard: 6
 
 
