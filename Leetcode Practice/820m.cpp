1b08d644e6e76196b0bb3729210b3c90c700cb9d
princekhj555 <princekhj555@gmail.com>
2022-06-21 18:00:34 +0530

    Day 392

---
+++ Leetcode Practice/820m.cpp
@@ -0,0 +1,16 @@
+//820. Short Encoding of Words
+class Solution {
+public:
+    int minimumLengthEncoding(vector<string>& words) {
+       unordered_set<string> s(words.begin(), words.end());
+
+		for (auto &e : s) {
+			for (int i = 1; i < e.size(); ++i) {
+				s.erase(e.substr(i));
+			}
+		}
+		int ans = 0;
+		for (auto &e : s) ans += e.size() + 1;
+		return ans; 
+    }
+};
--- README.md
+++ README.md
@@ -1,13 +1,13 @@
 # DSA
 
-#Day 391
+#Day 392
 
 Love babbar sheet
     108/450
     
 leetcode Questions   
 easy : 152
-medium: 221
+medium: 222
 hard: 31
 
  
