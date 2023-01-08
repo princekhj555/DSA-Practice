f222a8b2c28c2e52d925fb21aec4e79cd3c81771
princekhj555 <princekhj555@gmail.com>
2023-01-08 20:59:50 +0530

    2023_Day8

---
+++ Leetcode Practice/149h.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    int maxPoints(vector<vector<int>>& points) {
+        int n=points.size();
+        if(n<=2) return n;
+        int sol=2;
+        for(int i=0;i<n;i++){
+            for(int j=i+1;j<n;j++){
+                int total=2;
+                for(int k=0;k<n;k++){
+                    if(k!=i and k!=j){
+                       if( (points[j][1]-points[i][1])*(points[k][0]-points[i][0])==(points[k][1]-points[i][1])*(points[j][0]-points[i][0]))
+                        total++;
+                    }
+                }
+                sol=max(sol,total);
+            }
+        }
+        return sol;
+    }
+};
