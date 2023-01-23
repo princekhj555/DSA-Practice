39416566b6290a5416b272615ffb41a96d4b4840
princekhj555 <princekhj555@gmail.com>
2023-01-23 12:14:21 +0530

    2023_Day22

--- Leetcode Practice/997e.cpp
+++ Leetcode Practice/997e.cpp
@@ -26,3 +26,35 @@ public:
     return (label + 1);
     }
 };
+
+
+//*************************************************************************************/
+class Solution {
+public:
+    int findJudge(int N, vector<vector<int>>& trust) {
+        vector<int> trustCount(N+1, 0);
+    
+    // First pass: calculate trust count for each person
+    for (auto t : trust) {
+        trustCount[t[0]]--;
+        trustCount[t[1]]++;
+    }
+    
+    // Second pass: find the person with a trust count of N-1
+    for (int i = 1; i <= N; i++) {
+        if (trustCount[i] == N-1) {
+            // Check if this person does not trust anyone
+            bool isJudge = true;
+            for (auto t : trust) {
+                if (t[0] == i) {
+                    isJudge = false;
+                    break;
+                }
+            }
+            if (isJudge) return i;
+        }
+    }
+    
+    return -1;
+    }
+};
